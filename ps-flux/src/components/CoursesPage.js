a81e629dfda92c3fb18db88cebeccca6d45cfa89
itzkumaran <kumaran4jobs@gmail.com>
2021-11-15 23:18:26 -0500

    Props and Proptypes

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -1,25 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
-
-function renderHeader() {
-    return(
-    <tr>
-        <th>Title</th>
-        <th>Author ID</th>
-        <th>Category</th>
-    </tr>  
-    );      
-}
-
-function renderRow(course) {
-    return (
-        <tr key={course.id}>
-            <td>{course.title}</td>
-            <td>{course.authorId}</td>
-            <td>{course.category}</td>
-        </tr>
-    );        
-}
+import CourseList from "./CourseList";
 
 function CoursesPage () {
     const [courses, setCourses] = useState([]);
@@ -30,14 +11,7 @@ function CoursesPage () {
     return (
         <>
         <h2>Courses</h2>
-        <table className="table">
-            <thead>
-                { renderHeader() }
-            </thead>
-            <tbody> 
-                { courses.map(renderRow) }
-            </tbody>
-        </table>
+        <CourseList courses = {courses} />
         </>
     );
    }
